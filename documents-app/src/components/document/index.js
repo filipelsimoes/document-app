fd293b60cdb015d2e3cea843a7788d68607b136e
filipelsimoes <filipe.simoes@live.com.pt>
2022-08-18 00:41:54 +0100

    Fix component Document style with data from BE

--- documents-app/src/components/document/index.js
+++ documents-app/src/components/document/index.js
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function DocumentBox({document}) {
 
@@ -7,12 +8,16 @@ export default function DocumentBox({document}) {
   return (
     <View style={styles.container}>
         <View style={styles.titleContainer}>
-            <Text>{document.Title}</Text> 
-            <Text>{document.Version}</Text> 
+            <Text style={styles.titleLabel} >{document.Title}</Text> 
+            <Text style={styles.versionLabel}>Version {document.Version}</Text> 
         </View>
         <View style={styles.contributorsContainer}>
             <View>
+                <View style={styles.contributorsContainerTitle}>
+                    <Icon name="group" size={16} color="black"></Icon>
                 <Text>Contributors</Text>
+                </View>
+                
                 <View>
                     {document.Contributors.map((item) => {
                         return ( <Text>{item.Name}</Text>)
@@ -20,7 +25,10 @@ export default function DocumentBox({document}) {
                 </View>
             </View>
             <View>
-                <Text>Attachements</Text>
+                  <View style={styles.contributorsContainerTitle}>
+                    <Icon name="file" size={16} color="black"></Icon>
+                <Text>Attachments</Text>
+                </View>
                <View>
                     {document.Attachments.map((item) => {
                         return ( <Text>{item}</Text>)
@@ -49,12 +57,30 @@ const styles = StyleSheet.create({
     titleContainer: {
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'space-between'
     },
     contributorsContainer: {
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-between',
         marginTop: 10
+    },
+    contributorsContainerTitle: {
+        display: 'flex',
+        flexDirection: 'row',
+        marginTop: 10,
+        justifyContent: 'space-evenly',
+        width: '60%',
+        marginBottom: 10
+    },
+    titleLabel: {
+        fontWeight: '600',
+        marginRight: 5
+    }, 
+    versionLabel: {
+        fontWeight: '600',
+        marginRight: 5,
+        color: '#808080',
+        fontSize: 12,
+        alignSelf: 'center'
     }
 })
